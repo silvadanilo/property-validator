4143aeef75c3dc23deafefbdc83c913e5cd1a000
danilo silva <badkill82@gmail.com>
2020-02-09 23:40:06 +0100

    magic getter
    It is now possible to call the method with the name of the desired property as a getter

--- src/PropertyValidator.php
+++ src/PropertyValidator.php
@@ -13,6 +13,8 @@ trait PropertyValidator
 {
     private static $initiated = false;
 
+    private array $properties = [];
+
     public static function create(array $data = []): Self
     {
         self::init();
@@ -21,9 +23,10 @@ trait PropertyValidator
         $nullProperties = self::nullProperties($rc);
 
         $data = array_merge($nullProperties, $rc->getDefaultProperties(), $data);
+        $properties = array_keys($nullProperties);
 
         $rules = [];
-        foreach (array_keys($nullProperties) as $property) {
+        foreach ($properties as $property) {
             $rules[$property] = [];
             $type = $rc->getProperty($property)->getType();
             if ($type) {
@@ -57,11 +60,22 @@ trait PropertyValidator
         }
 
         $self = new Self();
+        $self->__properties = $properties;
+
         foreach ($data as $key => $value) {
             $self->$key = $value;
         }
 
         return $self;
+    }
+
+    public function __call($method, $args)
+    {
+        if (!in_array($method, $this->__properties)) {
+            throw \RuntimeException("`$method` field is missing in the " . __CLASS__ . ' object');
+        }
+
+        return $this->$method;
     }
 
     private static function nullProperties(ReflectionClass $rc): array
